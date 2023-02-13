703bc8d0bfeb98c0f0d653f6097d3b3e286fb573
sericaer <sericaer@navinfo.com>
2023-02-13 18:31:41 +0800

    Add UC1，UC2，UC3

---
+++ Assets/UC1.xaml.cs
@@ -0,0 +1,34 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+using Noesis;
+#else
+using System;
+using System.Windows;
+using System.Windows.Controls;
+#endif
+
+namespace KungFCollage
+{
+    /// <summary>
+    /// UC1.xaml 的交互逻辑
+    /// </summary>
+    public partial class UC1 : UserControl
+    {
+        public UC1()
+        {
+            InitializeComponent();
+
+            BT.Click += (sender, e) =>
+            {
+                this.Visibility = Visibility.Hidden;
+            };
+        }
+
+#if NOESIS
+        private void InitializeComponent()
+        {
+            NoesisUnity.LoadComponent(this);
+        }
+#endif
+    }
+}
---
+++ Assets/UC2.cs
@@ -0,0 +1,61 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+using Noesis;
+#else
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+#endif
+
+
+namespace KungFCollage
+{
+    [ContentProperty("Children")]
+    public class UC2 : UserControl
+    {
+        public UIElementCollection Children { get; set; }
+
+        public UC2()
+        {
+            //Grid grid = new Grid();
+
+            ////ContentPresenter content = new ContentPresenter();
+            ////content.Content = Content;
+
+            ////grid.Children.Add(content);
+
+            //var stackPanel = new StackPanel() { Orientation = Orientation.Horizontal };
+            //grid.Children.Add(stackPanel);
+
+            //var subGrid = new Grid();
+            //stackPanel.Children.Add(subGrid);
+
+            //var button = new Button() { Content = "X", Width = 30, Height = 30, VerticalAlignment = VerticalAlignment.Top };
+            //stackPanel.Children.Add(button);
+
+            //button.Click += (sender, e) =>
+            //{
+            //    this.Visibility = Visibility.Hidden;
+            //};
+
+            //Children = subGrid.Children;
+
+            //Content = grid;
+
+            var dock = new DockPanel();
+
+            var button = new Button() { Content = "X", Width = 30, Height = 30, VerticalAlignment = VerticalAlignment.Top, HorizontalAlignment = HorizontalAlignment.Right };
+            dock.Children.Add(button);
+
+            button.Click += (sender, e) =>
+            {
+                this.Visibility = Visibility.Hidden;
+            };
+
+            Children = dock.Children;
+
+            Content = dock;
+        }
+    }
+}
